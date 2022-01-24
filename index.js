9b6bba35895cfad04543419c7a2ee88a3225d999
Sean Martin <sean@seanmartinstudio.com>
2022-01-24 10:38:55 -0800

    Done.

--- index.js
+++ index.js
@@ -1,22 +1,47 @@
+
+
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+for (let i = 0; i < array.length; i++) {
+
+  const complement = target - array[i]
+
+  for (let j = i + i; j < array.length; j++) {
+
+    if (array[j] === complement) return true
+
+  }
 }
+
+return false
+}
+
+
+
+
+
 
 /* 
   Write the Big O time complexity of your function here
 */
 
 /* 
-  Add your pseudocode here
+Iterat through each number in the array
+  for the current num, edintify complemenent that adds to the target (compliment = target - num)
+  iterate throughthe rest of the array
+  check if any number is our compliement
+  if so, return true
+
+if I reach the end of the array, return False
 */
 
 /*
-  Add written explanation of your solution here
+Create a function that iterates through an array and finds the sum of every two number pair.
+If sum of two number pair is equal to the target nuymber, return true.
+If there are so two number pairs that's sum equals the target number, return false. 
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
 
@@ -27,8 +52,9 @@ if (require.main === module) {
 
   console.log("");
 
-  console.log("Expecting: false");
+  console.log("Expecting: false ***");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
 }
 
 module.exports = hasTargetSum;
+
